5bafa5027f5bb5ddca7eb70e89725f1ab0d147ab
sujit995 <sujitmishra402@gmail.com>
2021-11-27 10:18:58 +0530

    Posting article

--- server/routes/api/article.js
+++ server/routes/api/article.js
@@ -1,7 +1,21 @@
-const express  = require('express');
+const express = require('express');
 let router = express.Router();
 const { checkLoggedIn } = require('../../middleware/auth');
 const { grantAccess } = require('../../middleware/roles');
 
-// model 
 const { Article } = require('../../models/article_model');
+
+
+router.route('/admin/add_articles')
+    .post(checkLoggedIn, grantAccess('createAny', 'article'), async (req, res) => {
+        try {
+            const article = new Article({
+                ...req.body,
+                score: parseInt(req.body.score)
+            });
+            const result = await article.save();
+            res.status(200).json(result)
+        } catch (error) {
+            res.status(400).json({ message: 'Error adding article', error: error })
+        }
+    })
